ae845746b84f4d60746e43a75c3cc2c2bb69efda
prathammist <153816153+prathammist@users.noreply.github.com>
2025-02-15 19:12:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,147 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".container__left h1", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".container__left .container__btn", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".container__right h4", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+ScrollReveal().reveal(".container__right h2", {
+  ...scrollRevealOption,
+  delay: 2500,
+});
+ScrollReveal().reveal(".container__right h3", {
+  ...scrollRevealOption,
+  delay: 3000,
+});
+ScrollReveal().reveal(".container__right p", {
+  ...scrollRevealOption,
+  delay: 3500,
+});
+
+ScrollReveal().reveal(".container__right .tent-1", {
+  duration: 1000,
+  delay: 4000,
+});
+ScrollReveal().reveal(".container__right .tent-2", {
+  duration: 1000,
+  delay: 4500,
+});
+
+ScrollReveal().reveal(".location", {
+  ...scrollRevealOption,
+  origin: "left",
+  delay: 5000,
+});
+
+ScrollReveal().reveal(".socials span", {
+  ...scrollRevealOption,
+  origin: "top",
+  delay: 5500,
+  interval: 500,
+});
+// This is for Mouse Movement
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+const colors = [
+  "#ffb56b",
+  "#fdaf69",
+  "#f89d63",
+  "#f59761",
+  "#ef865e",
+  "#ec805d",
+  "#e36e5c",
+  "#df685c",
+  "#d5585c",
+  "#d1525c",
+  "#c5415d",
+  "#c03b5d",
+  "#b22c5e",
+  "#ac265e",
+  "#9c155f",
+  "#950f5f",
+  "#830060",
+  "#7c0060",
+  "#680060",
+  "#60005f",
+  "#48005f",
+  "#3d005e"
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+// imageMove
+const imageItems = document.querySelectorAll('.images');
+
+imageItems.forEach(item => {
+  item.addEventListener('mousemove', (e) => {
+    const { left, top, width, height } = item.getBoundingClientRect();
+    const x = (e.clientX - left - width / 2) * 0.1;
+    const y = (e.clientY - top - height / 2) * 0.1;
+
+    item.style.transform = `scale(1.1) rotate3d(${y / 100}, ${x / 100}, 0, 5deg) translate(${x}px, ${y}px)`;
+  });
+
+  item.addEventListener('mouseleave', () => {
+    item.style.transform = 'scale(1) rotate3d(0, 0, 0, 0deg) translate(0, 0)';
+  });
+});
